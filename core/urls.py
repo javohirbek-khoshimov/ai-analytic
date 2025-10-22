abeb77cbc25bbea06a149e6052adeb28868f93e7
Javohirbek Khoshimov <67156387+javohirbek-khoshimov@users.noreply.github.com>
2025-10-22 17:45:14 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,14 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # 1) To'g'ri Netlify domenini qo'shamiz
     allow_origins=[
-        "https://sinoanalize.netlify.app",   # <— sizning frontend
-        "https://sinoanalytic.netlify.app",  # (ixtiyoriy, eski nom bo‘lsa ham)
+        "https://sinoanaliz.netlify.app",  # <-- to‘g‘ri domen (analiz)
         "http://localhost:5500",
     ],
-    # yoki bitta regex bilan ham qo‘yish mumkin:
-    # allow_origin_regex=r"https://.*\.netlify\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
